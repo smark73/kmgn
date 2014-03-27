3f31bd3b7569644cf270f1a4c60c465a0c40e6d1
Stacy office dev <stacy.mark@kaff.com>
2014-03-27 16:39:43 -0700

    css fine tuning

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0fc0c00ab5d4575a4efcc8fad9aba157');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f876e03e0bc42fe0c5ee2a6f5d8d047');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
