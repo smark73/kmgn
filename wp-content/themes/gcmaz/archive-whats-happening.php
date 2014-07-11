5747ff187840d43c08421cfb9feedcc4f02c09ec
Stacy at home <stacy@stacymark.com>
2014-07-11 13:04:59 -0700

    fix firefox bug with hr's on archive pages

--- wp-content/themes/gcmaz/archive-whats-happening.php
+++ wp-content/themes/gcmaz/archive-whats-happening.php
@@ -20,23 +20,22 @@
             $fdate = get_post_custom_values('whats_fulldate');
             if(($fdate[0] == null) || (strtotime($fdate[0])) >= (strtotime('now'))) : ?>
                 <section class="archv-pg-lstng row">
-                <?php if(has_post_thumbnail()) : ?>
-                    <div class="archv-thmb col-md-3 col-sm-4 hidden-xs">
-                        <?php the_post_thumbnail('thumbnail');?>
+                    <?php if(has_post_thumbnail()) : ?>
+                        <div class="archv-thmb col-md-3 col-sm-4 hidden-xs">
+                            <?php the_post_thumbnail('thumbnail');?>
+                        </div>
+                        <div class="centered visible-xs">
+                            <?php the_post_thumbnail('thumbnail');?>
+                       </div>
+                    <?php endif;?>
+                    <div class="archv-info col-md-9 col-sm-8 col-xs-12">
+                    <span class="archv-date pull-right red">
+                        <?php $wdate = get_post_custom_values('whats_date'); echo $wdate[0];?>
+                    </span>
+                        <?php get_template_part('templates/content', get_post_format());?>
                     </div>
-                    <div class="centered visible-xs">
-                        <?php the_post_thumbnail('thumbnail');?>
-                   </div>
-                <?php endif;?>
-                <div class="archv-info col-md-9 col-sm-8 col-xs-12">
-                <span class="archv-date pull-right red">
-                    <?php $wdate = get_post_custom_values('whats_date'); echo $wdate[0];?>
-                </span>
-                    <?php get_template_part('templates/content', get_post_format());?>
-                </div>
-                <div class="clearfix">
-                <hr class="archv-pg-hr">
-                </div>
+                    <div class="clearfix"></div>
+                    <hr class="archv-pg-hr">
                 </section>
             <?php endif;?>
     
