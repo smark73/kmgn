797cb15f39b73a5e91cd88ad593ea5a7d7fe088e
Stacy at home <stacy@stacymark.com>
2014-08-25 13:06:08 -0700

    updated sidebar template for new feed pages

--- wp-content/themes/gcmaz/templates/sidebar.php
+++ wp-content/themes/gcmaz/templates/sidebar.php
@@ -1,11 +1,11 @@
 <?php
 if($post->post_title == 'Home'){
     dynamic_sidebar('sidebar-homepage');
-} elseif($post->post_type == 'concert'){
+} elseif($post->post_type == 'concert' || $post->post_name == 'concerts-northern-arizona'){
     dynamic_sidebar('sidebar-concert');
-} elseif($post->post_type == 'whats-happening'){
+} elseif($post->post_type == 'whats-happening' || $post->post_name == 'whats-happening-northern-arizona'){
     dynamic_sidebar('sidebar-whats');
-} elseif($post->post_type == 'community-info'){
+} elseif($post->post_type == 'community-info' || $post->post_name == 'northern-arizona-community-info'){
     dynamic_sidebar('sidebar-community');
 } elseif($post->post_title == 'On Air Shows'){
     dynamic_sidebar('sidebar-onair');
