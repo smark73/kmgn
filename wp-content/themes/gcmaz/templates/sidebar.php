6b7e9c086133109930935e54a861d90fa5d94a54
Stacy at home <stacy@stacymark.com>
2015-01-14 14:33:59 -0800

    updated sidebar tpl

--- wp-content/themes/gcmaz/templates/sidebar.php
+++ wp-content/themes/gcmaz/templates/sidebar.php
@@ -1,5 +1,6 @@
 <?php
-if($post->post_title == 'Home'){
+global $post;
+if(is_front_page()){
     dynamic_sidebar('sidebar-homepage');
 } elseif($post->post_type == 'concert' || $post->post_name == 'concerts-northern-arizona'){
     dynamic_sidebar('sidebar-concert');
@@ -7,9 +8,9 @@ if($post->post_title == 'Home'){
     dynamic_sidebar('sidebar-whats');
 } elseif($post->post_type == 'community-info' || $post->post_name == 'northern-arizona-community-info'){
     dynamic_sidebar('sidebar-community');
-} elseif($post->post_title == 'On Air Shows'){
+} elseif($post->post_name == 'radio-shows'){
     dynamic_sidebar('sidebar-onair');
-} elseif($post->post_title == 'Area Weather'){
+} elseif($post->post_name == 'weather'){
     dynamic_sidebar('sidebar-weather');
 } elseif($post->post_name == 'song-requests'){
     dynamic_sidebar('sidebar-songrequests');
