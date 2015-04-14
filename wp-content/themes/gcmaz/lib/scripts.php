f1aa64712fed3fb567a67f7398043658fa22b27b
Stacy at home <stacy@stacymark.com>
2015-04-14 08:37:34 -0700

    update bg colors css

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '98debccb3d9ece7185c2a48b385a0a6a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '521859b2676dbfaf0b195b43f133d2eb');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
