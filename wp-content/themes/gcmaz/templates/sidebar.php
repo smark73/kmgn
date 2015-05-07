575e1fd6e2cc4b0ea02b6660e2e213709dfb648e
Stacy at home <stacy@stacymark.com>
2015-05-07 12:59:13 -0700

    fix wrong sidebar when search in sidebar.php

--- wp-content/themes/gcmaz/templates/sidebar.php
+++ wp-content/themes/gcmaz/templates/sidebar.php
@@ -1,22 +1,43 @@
 <?php
 global $post;
+
 if(!empty( $post )){
-    if(is_front_page()){
+    // need to keep is_search check first ... otherwise gets wrong sidebar as search info moves into post->info (a search for "concerts" calls the concerts sidebar)
+    if( is_search() ) {
+        //echo "<div style='background:yellow'>search</div>";
+        dynamic_sidebar('sidebar-primary');
+        
+    } elseif(is_front_page()){
+        //echo "<div style='background:yellow'>front page</div>";
         dynamic_sidebar('sidebar-homepage');
+        
     } elseif($post->post_type == 'concert' || $post->post_name == 'concerts-northern-arizona'){
+        //echo "<div style='background:yellow'>concert</div>";
         dynamic_sidebar('sidebar-concert');
+        
     } elseif($post->post_type == 'whats-happening' || $post->post_name == 'whats-happening-northern-arizona'){
+        //echo "<div style='background:yellow'>whats</div>";
         dynamic_sidebar('sidebar-whats');
+        
     } elseif($post->post_type == 'community-info' || $post->post_name == 'northern-arizona-community-info'){
+        //echo "<div style='background:yellow'>community</div>";
         dynamic_sidebar('sidebar-community');
+        
     } elseif($post->post_name == 'radio-shows'){
+        //echo "<div style='background:yellow'>on air</div>";
         dynamic_sidebar('sidebar-onair');
+        
     } elseif($post->post_name == 'weather'){
+        //echo "<div style='background:yellow'>weather</div>";
         dynamic_sidebar('sidebar-weather');
+        
     } elseif($post->post_name == 'song-requests'){
+        //echo "<div style='background:yellow'>requests</div>";
         dynamic_sidebar('sidebar-songrequests');
+        
     } else {
         dynamic_sidebar('sidebar-primary');
     }
+    
 }
 ?>
