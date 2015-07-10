645b7e146b57960fe322069b724792253150c5f9
Stacy at home <stacy@stacymark.com>
2015-07-10 07:43:46 -0700

    change exp-leaderboard tpl to use guids

--- wp-content/themes/gcmaz/templates/exp-leaderboard.php
+++ wp-content/themes/gcmaz/templates/exp-leaderboard.php
@@ -5,13 +5,39 @@
 
 global $post;
 if(!empty( $post )){
-    if($post->post_title == 'Home'){
+    
+    //compare the guid against our known ones
+    //get current page/post guid
+    $p_guid = get_the_guid($post->ID);
+    
+    //get the position of numbers after equal sign
+    $guid_num_pos = strpos( $p_guid, "=" ) + 1;
+    //get the numbers only from the guid
+    $guid_num = substr( $p_guid, $guid_num_pos );
+
+    //get the domain name & populate our guids (different guids on .dev and .com)
+    if( strpos( $_SERVER['HTTP_HOST'], '.dev') !== false ){
+        //on .dev site
+        $guid_whats = 239;
+        $guid_comm = 236;
+        $guid_concerts = 241;
+    } else {
+        // on .com site
+        $guid_whats = 513;
+        $guid_comm = 509;
+        $guid_concerts = 511;
+    }
+    
+    if( is_front_page() ){
         $groupnum = 10;
-    } elseif($post->post_type == 'whats-happening') {
+    //} elseif($post->post_type == 'whats-happening') {
+    } elseif( $guid_num == $guid_whats ) {
         $groupnum = 11;
-    } elseif($post->post_type == 'community-info') {
+    //} elseif($post->post_type == 'community-info') {
+    } elseif( $guid_num == $guid_comm ) {
         $groupnum = 12;
-    } elseif($post->post_type == 'concert') {
+    //} elseif($post->post_type == 'concert') {
+    } elseif( $guid_num == $guid_concerts ) {
         $groupnum = 13;
     }
     
