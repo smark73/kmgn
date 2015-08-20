aa6c92e5201ce03c23d814d40e162d53d83e7487
Stacy at home <stacy@stacymark.com>
2015-08-20 12:32:01 -0700

    update for deprecated __constructor

--- wp-content/themes/gcmaz/lib/widgets.php
+++ wp-content/themes/gcmaz/lib/widgets.php
@@ -107,7 +107,10 @@ class Roots_Vcard_Widget extends WP_Widget {
   function __construct() {
     $widget_ops = array('classname' => 'widget_roots_vcard', 'description' => __('Use this widget to add a vCard', 'roots'));
 
-    $this->WP_Widget('widget_roots_vcard', __('Roots: vCard', 'roots'), $widget_ops);
+    // DEPRECATED Constructor - causes errors
+    //$this->WP_Widget('widget_roots_vcard', __('Roots: vCard', 'roots'), $widget_ops);
+    // NEW
+    parent::__construct('widget_roots_vcard', __('Roots: vCard', 'roots'), $widget_ops);
     $this->alt_option_name = 'widget_roots_vcard';
 
     add_action('save_post', array(&$this, 'flush_widget_cache'));
