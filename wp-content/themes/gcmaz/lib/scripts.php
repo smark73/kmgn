1f7a53f75481a949c728c49bcb48c5400bdedb24
Stacy at home <stacy@stacymark.com>
2015-09-23 09:53:12 -0700

    added instagram icon

--- wp-content/themes/gcmaz/lib/scripts.php
+++ wp-content/themes/gcmaz/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cc68d57e4347852cc9d5f48912335b51');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'af7641f9998defc826a86217e23fac5b');
   wp_enqueue_style('nivo-slider', get_template_directory_uri() . '/assets/js/nivo-slider/nivo-slider.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
--- wp-content/themes/gcmaz/templates/navbar-icons.php
+++ wp-content/themes/gcmaz/templates/navbar-icons.php
@@ -5,4 +5,7 @@
     <a href="http://www.twitter.com/939TheMountain" target="_blank" class="icon-tw">
         <img src="/media/icon-nav-twtr.png" width="24" height="24" alt="93-9 The Mountain on Twitter" />
     </a>
+    <a href="https://instagram.com/939themountain/" target="_blank" class="icon-instgrm">
+        <img src="/media/icon-nav-instgrm.png" width="24" height="24" alt="93-9 The Mountain on Instagram" />
+    </a>
 </span>
